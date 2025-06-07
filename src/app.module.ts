139226212cf134b7910aee1f5abfd78e36bec037
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-07 17:19:20 +0300

    modules and json files configrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookController } from './books/books.controller';
+import { BookService } from './books/books.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BooksModule],
+  controllers: [AppController, BookController],
+  providers: [AppService, BookService],
 })
 export class AppModule {}
