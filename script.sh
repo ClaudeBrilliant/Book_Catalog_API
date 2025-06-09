007d7c2ac696136556b7c27b59dd433b7443bde0
ClaudeBrilliant <claudennyongesa7@gmail.com>
2025-06-09 21:28:36 +0300

    updated script file

--- script.sh
+++ script.sh
@@ -9,13 +9,12 @@ psql -U postgres -h localhost -c "CREATE DATABASE books_catalog;"
 psql -U postgres -h localhost -d books_catalog -f src/database/migrations/001_initial_schema.sql
 
 # CREATE stored procedures
-psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_create_books.sql
+psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_create_book.sql
 psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_count_by_publication_year.sql
 psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_get_books.sql
-psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_update_books.sql
-psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_delete_books.sql
+psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_update_book.sql
 psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_get_book_by_id.sql
-psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_hard_delete_books.sql
+psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_hard_delete_book.sql
 psql -U postgres -h localhost -d books_catalog -f src/database/procedures/sp_soft_delete_book.sql
 
 echo "DATABASE setup complete..."
